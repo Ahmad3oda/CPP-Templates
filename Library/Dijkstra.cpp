6dc6778ad6aff85edee599f8efd5125c312cf394
Ahmad Ouda <104653414+Ahmad3oda@users.noreply.github.com>
2025-07-25 23:14:43 +0300

    Update Dijkstra.cpp

--- Library/Dijkstra.cpp
+++ Library/Dijkstra.cpp
@@ -1,55 +1,40 @@
-//
-// Created by STW on 14-Dec-24.
-//
+#include <bits/stdc++.h>
+using namespace std;
 
 
+// DIJKSTRA
+#define pii pair <int,int>
 
+const int INF = 1e9;
+int n;
+vector<int> dis, par;
+vector<vector<pair<int, int>>> adj;
 
-/// DIJKSTRA
+void dijkstra(int src) {
+    dis.assign(n + 1, INF);
+    par.assign(n + 1, -1);
+    priority_queue<pii, vector<pii>, greater<>> pq;
 
+    dis[src] = 0;
+    pq.emplace(0, src);
 
+    while (!pq.empty()) {
+        auto [d, u] = pq.top(); pq.pop();
+        if (d > dis[u]) continue;
 
-// const int INF = 1e9;
-// int n;
-// vector<int> dis, par;
-// vector<vector<pair<int, int>>> adj;
-//
-// void dijkstra(int src) {
-//     dis.assign(n, INF);
-//     par.assign(n, -1); // initialize the par array
-//     priority_queue<pair<int, int>, vector<pii>, greater<pii>> pq;
-//
-//     dis[src] = 0;
-//     pq.push(make_pair(0, src));
-//
-//     while (!pq.empty()) {
-//         int d = pq.top().first;
-//         int u = pq.top().second;
-//         pq.pop();
-//
-//         if (d < dis[u]) continue;
-//         for (auto &[v, w] : adj[u]) {
-//
-//             if (dis[u] + w < dis[v]) {
-//                 dis[v] = dis[u] + w;
-//                 par[v] = u;
-//                 pq.emplace(dis[v], v);
-//             }
-//         }
-//     }
-// }
+        for (auto &[v, w] : adj[u])
+            if (dis[u] + w < dis[v]) {
+                dis[v] = dis[u] + w;
+                par[v] = u;
+                pq.emplace(dis[v], v);
+            }
+    }
+}
 
-
-/// GET PATH
-
-
-// vector<int> get_path(int src, int u) {
-//     vector<int> path;
-//     while (u != src) {
-//         path.push_back(u);
-//         u = par[u];
-//     }
-//     path.push_back(u);
-//     reverse(path.begin(), path.end());
-//     return path;
-// }
+vector<int> get_path(int src, int u) {
+    if (dis[u] == INF) return {};
+    vector<int> path;
+    while (u != -1) path.push_back(u), u = par[u];
+    reverse(path.begin(), path.end());
+    return path;
+}
